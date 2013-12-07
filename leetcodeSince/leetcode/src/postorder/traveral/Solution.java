9c81db930150ace703bbc7739ddeac82d7bbb456
Tim Kou <osu.flyer2010@gmail.com>
2013-12-06 22:51:19 -0500

    added is Same tree

--- leetcodeSince/leetcode/src/postorder/traveral/Solution.java
+++ leetcodeSince/leetcode/src/postorder/traveral/Solution.java
@@ -36,6 +36,25 @@ public class Solution {
     	
     }
     
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+    	
+        if(p==null && q ==null) return true;
+        else if(p==null && q !=null || q==null && p !=null)return false;
+        else if(p.val !=q.val ){
+            return false;
+        }else{
+          boolean left = isSameTree(p.left,q.left) ;
+          boolean right = isSameTree(p.right,q.right);
+          if(!left)return false;
+          if(!right)return false;
+          
+          return true;
+    	}
+    	
+        
+    }
+    
     public int maxDepth2(TreeNode root) {
     	if(root == null)return 0;
     	TreeNode cur=root;
